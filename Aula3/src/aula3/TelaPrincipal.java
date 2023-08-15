085efbee6687bab8e52327a85a84eebea198a41a
Gelo Marchini <gelo.marchini@gmail.com>
2023-08-14 21:32:08 -0300

    teste versionamento

--- Aula3/src/aula3/TelaPrincipal.java
+++ Aula3/src/aula3/TelaPrincipal.java
@@ -119,6 +119,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+             
                 new TelaPrincipal().setVisible(true);
             }
         });
